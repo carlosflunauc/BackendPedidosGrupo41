ae18f7eed1f70c14223381e8cf801323b2fd8bd2
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-03 06:51:11 -0500

    creacion de modelo pedido, persona
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,46 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  estado: string;
+
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
